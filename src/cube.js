60983d7ea782e4533fb59bdd46a980c09e2c152d
alan <alan001tk@gmail.com>
2022-11-01 22:33:30 -0700

    Getting somewhere, working on x collision

--- src/cube.js
+++ src/cube.js
@@ -27,7 +27,10 @@ let xDiff = 0.13;
 let velX = 0;
 let yDiff = 0.13;
 let velY = 0;
+let zDiff = 0.13;
+let velZ = 0;
 let grav = -0.03;
+/* let grav = 0; */
 let rClock = 0;
 let rCounterClock = 0;
 let friction = 0.9;
@@ -72,6 +75,40 @@ function moveCube(arr) {
       }
     }
   }
+  if (keys['s']) {
+    // cube.position.y += yDiff;
+    if (!reverse) {
+      if (velZ < zDiff) {
+        velZ += 0.03;
+      }
+    } else {
+      if (velZ > -zDiff) {
+        velZ -= 0.03;
+      }
+    }
+  }
+  if (keys['w']) {
+    // cube.position.y -= yDiff;
+    if (!reverse) {
+      if (velZ > -zDiff) {
+        velZ -= 0.03;
+      }
+    } else {
+      if (velZ < zDiff) {
+        velZ += 0.03;
+      }
+    }
+  }
+
+  if (keys[' ']) {
+    velY = 0.6;
+    if (velZ > 0) {
+      rCounterClock += 55;
+    }
+    if (velZ < 0) {
+      rClock += 55;
+    }
+  }
   if (keys['r']) {
     cube.position.set(-15, -3, 0);
     line.position.set(-15, -3, 0);
@@ -79,109 +116,75 @@ function moveCube(arr) {
     velY = 0;
   }
 
+  const checkCollision = (cubeBB, elBB) => {
+    if (cubeBB.intersectsBox(elBB)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Returns an array of the Vector3's values
+  const toArray = (cubeBB, elBB) => {
+    let arr = [0, 0, 0]
+    /* debugger */
+    for (let i = 0; i < 3; i++) {
+      let cubeMin = cubeBB.min.getComponent(i)
+      let cubeMax = cubeBB.max.getComponent(i)
+      let elMin = elBB.min.getComponent(i)
+      let elMax = elBB.max.getComponent(i)
+
+      if (cubeMin < elMax && cubeMax > elMax) {
+        // If left (-x) side of the cube is touching
+        arr[i] = -1;
+      } else if (cubeMin < elMin && cubeMax > elMax) {
+        arr[i] = 1;
+      }
+    }
+    return arr
+  }
+
+  const checkDir = (dirs) => {
+    let velDirs = [velX, velY, velZ];
+    console.log(dirs)
+    dirs.forEach((val, i) => {
+      if (val > 0 && velDirs[i] > 0) {
+        velDirs[i] = 0;
+      } else if (val < 0 && velDirs[i] < 0) {
+        velDirs[i] = 0;
+      }
+    })
+
+    velX = velDirs[0];
+    velY = velDirs[1];
+    velZ = velDirs[2];
+
+  }
+
   // Collision detection
+  let cubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+  cubeBB.setFromObject(cube);
   const cubeWidth = cube.geometry.parameters['width'];
   const cubeHeight = cube.geometry.parameters['height'];
   // const dispMulti = 0.23;
-  arr.forEach(el => {
-    // Collision on x axis
-    let cubeCenter = new THREE.Vector3();
-    cubeBBox.getCenter(cubeCenter);
-    let elCenter = new THREE.Vector3();
-    el.getCenter(elCenter);
-    
-    if (cubeCenter.z > el.max.z || 
-    cubeCenter.z < el.min.z) {
-      return;
+  arr.forEach(elBB => {
+    // Cube's bounding box position is updated
+    cubeBB.copy( cube.geometry.boundingBox).applyMatrix4(cube.matrixWorld);
+
+    // The element's are already bounding boxes
+    let isColliding = checkCollision(cubeBB, elBB);
+
+    // If they are colliding
+    if (isColliding) {
+      // Build the dirs to stop array
+      let dirs = toArray(cubeBB, elBB)
+
+      // Iterate through dir and stop if that direction is non-zero
+      checkDir(dirs);
     }
 
-    
-    // Still problem where half of the box can clip through edges, and
-    // tunneling can also occur
-    if (cubeBBox.intersectsBox(el)) {
-      if (el === finishBBox) {
-        finished = true;
-      }
-      cube.rotation.z = 0;
-      line.rotation.z = 0;
-      rClock = 0;
-      rCounterClock = 0;
 
-
-      let interPoint1 = cubeCenter.clone();
-      let interPoint2 = cubeCenter.clone();
-      let interPoint3 = cubeCenter.clone();
-      let interPoint4 = cubeCenter.clone();
-      interPoint1.y = cubeBBox.max.y;
-      interPoint2.y = cubeBBox.min.y;
-      interPoint3.y = cubeBBox.min.y;
-      interPoint4.y = cubeBBox.max.y;
-      if (cubeCenter.y > elCenter.y) {
-        interPoint2.y = el.max.y;
-        interPoint3.y = el.max.y;
-      } else {
-        interPoint1.y = el.min.y;
-        interPoint4.y = el.min.y;
-      }
-
-      if (cubeCenter.x > elCenter.x) {
-        interPoint1.x = el.min.x;
-        interPoint2.x = el.min.x;
-      } else {
-        interPoint3.x = el.max.x;
-        interPoint4.x = el.max.x;
-      }
-      if (cubeCenter.x < elCenter.x ) {
-        if (velX > 0
-        && !(el.containsPoint(interPoint1) || el.containsPoint(interPoint2))
-        && (el.containsPoint(interPoint3) || el.containsPoint(interPoint4))) {
-          velX = 0;
-        }      
-      } else if (cubeCenter.x > elCenter.x ) {
-        if (velX < 0
-        && !(el.containsPoint(interPoint3) || el.containsPoint(interPoint4))
-        && (el.containsPoint(interPoint1) || el.containsPoint(interPoint2))) {
-          velX = 0;
-        }
-      }
-      if (cubeCenter.y > elCenter.y
-      && !(el.containsPoint(interPoint1) || el.containsPoint(interPoint4))
-      && (el.containsPoint(interPoint2) && el.containsPoint(interPoint3))) {
-        if (velY < 0) {
-          velY = 0;
-        } 
-      } else if (cubeCenter.y < elCenter.y
-      && !(el.containsPoint(interPoint2) || el.containsPoint(interPoint3))) {
-        if (velY > 0) {
-          velY = 0;
-        }
-      }
-      // Jump if we are touching the ground
-      if (keys[' '] && (el.containsPoint(interPoint2) && el.containsPoint(interPoint3))) {
-        velY = 0.6;
-        if (velX > 0) {
-          rClock += 105;
-        }
-        if (velX < 0) {
-          rCounterClock += 105;
-        }
-      }
-
-
-      if (el.containsPoint(interPoint1) && el.containsPoint(interPoint2)) {
-        cube.position.x += (cubeWidth / 2) - Math.abs(cubeCenter.x - el.max.x) - 0.01;
-      }
-      if (el.containsPoint(interPoint3) && el.containsPoint(interPoint4)) {
-        cube.position.x -= (cubeWidth / 2) - Math.abs(cubeCenter.x - el.min.x) - 0.01;
-      }
-      if (el.containsPoint(interPoint2) && el.containsPoint(interPoint3)) {
-        cube.position.y += (cubeHeight / 2) - (cubeCenter.y - el.max.y) - 0.01;
-      }
-      if (el.containsPoint(interPoint1) && el.containsPoint(interPoint4)) {
-        cube.position.y -= (cubeHeight / 2) - (cubeCenter.y - el.min.y) - 0.01;
-      }
-    }
-
+    // Cube rotation
     if (rClock) {
       cube.rotation.z -= 0.03;
       line.rotation.z -= 0.03;
@@ -207,9 +210,14 @@ function moveCube(arr) {
   if (Math.abs(velX) < 0.03) {
     velX = 0;
   }
+  if (Math.abs(velZ) < 0.03) {
+    velZ = 0;
+  }
   velX *= friction;
   velY *= friction;
+  velZ *= friction;
   
+  cube.position.z += velZ;
   cube.position.x += velX;
   cube.position.y += velY;
   line.position.x = cube.position.x;
@@ -217,7 +225,7 @@ function moveCube(arr) {
   updateCubeBBox();
 }
 
-let zDiff = 0.3;
+/* let zDiff = 0.3;
 let velZ = 0;
 function moveCubeAlt(arr) {
   finished = false;
@@ -397,7 +405,7 @@ function moveCubeAlt(arr) {
   line.position.z = cube.position.z;
   line.position.y = cube.position.y;
   updateCubeBBox();
-}
+} */
 
 // Bounding box stuff
 cube.geometry.computeBoundingBox();
@@ -408,4 +416,5 @@ function updateCubeBBox() {
 }
 
 
-export { cube, line, keys, moveCube, cubeBBox, updateCubeBBox, moveCubeAlt, finished, stop };
+/* export { cube, line, keys, moveCube, cubeBBox, updateCubeBBox, moveCubeAlt, finished, stop }; */
+export { cube, line, keys, moveCube, cubeBBox, updateCubeBBox, finished, stop };
